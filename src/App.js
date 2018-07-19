b0590be02a03dc6e5f3600dce749d854711c66c8
aslihanozfidan <aslihanozfidan@gmail.com>
2018-07-19 22:39:39 +0300

    deleted fontawesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faStroopwafel)
-
 import SearchBox from './Components/SearchBox/SearchBox';
 import Header from './Components/Header/Header';
 
