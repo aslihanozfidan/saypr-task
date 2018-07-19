af74128c2bc777e170a0cbf437575a54e3a5be59
aslihanozfidan <aslihanozfidan@gmail.com>
2018-07-20 00:58:07 +0300

    created item state in search box component

--- src/Components/SearchBox/SearchBox.js
+++ src/Components/SearchBox/SearchBox.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import './SearchBox.css';
 
 class SearchBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: '',
+            itemList: [],
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ item: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState(prevState => ({
+            itemList: [...prevState.itemList, this.state.item]
+        }));
+        console.log(this.state.item);
+    }
+
     render() {
         return (
             <div className="SearchBox">
-                <input type="text" placeholder="We want to go viral..." />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="We want to go viral..." value={this.state.item} onChange={this.handleChange} />
+                    <input type="submit" value="Submit" />
+                </form>
                 <span className="max-character">0/75</span>
                 <div className="label-area">
                     <div className="label">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
